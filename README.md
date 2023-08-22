35c83d6458409f35e00ed8d3986b6fe4ad2e001d
Eggshrinker <71610874+Eggshrinker@users.noreply.github.com>
2023-08-22 15:57:02 +0200

    second home work

--- README.md
+++ README.md
@@ -3,18 +3,15 @@ cash library for cashing key, value data
 
 how it works:
 ```go
-package main
-
-import (
-	"fmt"
-	"github.com/DenisFilisov/cacshLib"
-)
-
 func main() {
-	cacshLib.Set("1", "test")
-	fmt.Println(cacshLib.Get("1"))
-	cacshLib.Delete("1")
-	fmt.Println(cacshLib.Get("1"))
+cacshLib.Set("1", time.Second*5, "1")
+cacshLib.Set("2", time.Second*1, "2")
+cacshLib.Set("3", time.Second*4, "4")
+
+time.Sleep(time.Second * 2)
+fmt.Println(cacshLib.Get("1"))
+fmt.Println(cacshLib.Get("2"))
+fmt.Println(cacshLib.Get("3"))
 }
 
 ```
--- cashLib.go
+++ cashLib.go
@@ -1,15 +1,32 @@
 package cacshLib
 
+import (
+	"errors"
+	"time"
+)
+
 var myMap = make(map[string]interface{})
 
-func Set(key string, value interface{}) {
+func Set(key string, ttl time.Duration, value interface{}) {
 	myMap[key] = value
+	go worker(ttl, key)
 }
 
 func Get(key string) interface{} {
-	return myMap[key]
+	value := myMap[key]
+	if value == nil {
+		return errors.New("Key didn't find")
+	}
+	return value
 }
 
 func Delete(key string) {
 	delete(myMap, key)
+}
+
+func worker(ttl time.Duration, key string) {
+	time.Sleep(ttl)
+	if Get(key) != nil {
+		Delete(key)
+	}
 }
