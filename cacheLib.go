c7af163df173f2121e8c25526b4c0da230a92cb7
Denis <DFilisov@alfabank.ru>
2024-03-12 15:14:00 +0300

    thread safe cache

--- cacheLib.go
+++ cacheLib.go
@@ -2,31 +2,75 @@ package cacheLib
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
-var myMap = make(map[string]interface{})
-
-func Set(key string, ttl time.Duration, value interface{}) {
-	myMap[key] = value
-	go worker(ttl, key)
+type Cache struct {
+	cache map[string]Profile
+	mu    sync.RWMutex
+	ttl   time.Duration
 }
 
-func Get(key string) interface{} {
-	value := myMap[key]
-	if value == nil {
+type Profile struct {
+	Data    string
+	SysInfo SysInfo
+}
+
+type SysInfo struct {
+	CreatedAt time.Time
+}
+
+func NewCache(ttl time.Duration) *Cache {
+	cache := &Cache{
+		cache: make(map[string]Profile),
+		mu:    sync.RWMutex{},
+		ttl:   ttl,
+	}
+	go cache.StartCleanUp()
+	return cache
+}
+
+func (c *Cache) StartCleanUp() {
+	ticker := time.NewTicker(c.ttl)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			c.Clean()
+		}
+	}
+}
+
+func (c *Cache) Set(key string, value Profile) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	value, ok := c.cache[key]
+	if !ok {
 		return errors.New("Key didn't find")
 	}
 	return value
 }
 
-func Delete(key string) {
-	delete(myMap, key)
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.cache, key)
 }
 
-func worker(ttl time.Duration, key string) {
-	time.Sleep(ttl)
-	if Get(key) != nil {
-		Delete(key)
+func (c *Cache) Clean() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	now := time.Now()
+	for id, profile := range c.cache {
+		if now.Sub(profile.SysInfo.CreatedAt) >= c.ttl {
+			delete(c.cache, id)
+		}
 	}
 }
